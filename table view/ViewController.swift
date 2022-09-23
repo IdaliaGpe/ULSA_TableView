cfb4dd9ad7fa489f7746b0b6db6a354cf9b548f5
Alumno <cesarrgomez@hotmail.com>
2022-09-23 07:20:04 -0700

    Initial Commit

---
+++ table view/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  table view
+//
+//  Created by Alumno on 9/23/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
